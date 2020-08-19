8e4d051fd4c9e351e8ebe6db2b7e55f1c39b36ba
James Meixner <68332274+zocrateez@users.noreply.github.com>
2020-08-19 15:51:01 +0200

    Update Network.py

--- Network.py
+++ Network.py
@@ -6,7 +6,7 @@ import json
 class Network:
     def __init__(self):
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.server = "192.168.178.138"
+        self.server = ""    # Insert IP-adress.
         self.port = 5555
         self.adress = (self.server, self.port)
         self.id = self.connect()
